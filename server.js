e07b80eb2d59660e54961499f158dc09f18181ab
Nolan Knill <nolanknill@gmail.com>
2024-03-06 10:46:48 -0500

    Adds GET /pets endpoint

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express();
 
+app.get("/pets", (req, res) => {
+    return res.json([
+        "Sophie",
+        "Zeus",
+        "Leeland",
+        "Kermit"
+    ]);
+})
+
 app.listen(8080, () => {
     console.log("App is listening on http://localhost:8080");
 })
